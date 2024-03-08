8101fe7e00875f4b5446619108ebd6d6ba540b33
blvu <013ayu@gmail.com>
2024-03-08 14:45:03 +0800

    remove loader color

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -3,7 +3,7 @@ import { View, StyleSheet, ActivityIndicator } from "react-native"
 export const Loader = () => {
     return (
         <View style={styles.loading}>
-            <ActivityIndicator  size="large" color="#0000ff" />
+            <ActivityIndicator  size="large" />
         </View>
     )
 }
