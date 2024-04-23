e18a651f48297b10f13957d382438da39952a551
blvu <013ayu@gmail.com>
2024-04-23 22:49:37 +0800

    removed unused imports

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,7 +7,7 @@ import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
 import { Feather } from '@expo/vector-icons';
-import { useCamera } from '../contexts/CameraContext';
+import { useCamera } from '@/contexts/CameraContext';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
