86776aeda5c9845ed12d30a709760b1944cbeb1c
blvu <013ayu@gmail.com>
2024-05-02 02:27:52 +0800

    add placeholder component

---
+++ components/InDevelopment.tsx
@@ -0,0 +1,11 @@
+import { YStack, H1, Text } from "tamagui";
+
+
+export function InDevelopment() {
+    return (
+        <YStack alignItems="center">
+            <H1>Coming Soon</H1>
+            <Text theme="alt2">Page in development</Text>
+        </YStack>
+    )
+}
