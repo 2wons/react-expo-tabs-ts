5cdf1547fbbccc01496ac89aa7a91b8fce84e078
blvu <013ayu@gmail.com>
2024-05-16 05:59:01 +0800

    feat; get user current location

--- app/(tabs)/maps.tsx
+++ app/(tabs)/maps.tsx
@@ -1,12 +1,49 @@
-import { StyleSheet } from 'react-native';
+import { Alert, StyleSheet } from 'react-native';
 import { View } from '@/components/Themed';
 
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import * as Location from 'expo-location'
+import { useState, useEffect } from 'react';
+
+const INITIAL_REGION = {
+  /* Feut */
+  latitude: 14.604326629763175,
+  longitude: 120.98867833889382,
+  latitudeDelta: 0.00422,
+  longitudeDelta: 0.00421 
+}
 
 export default function MapScreen() {
+  const [location, setLocation] = useState<Location.LocationObject>()
+  
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync()
+      if (status != 'granted') {
+        Alert.alert("Permission to access location was denied.")
+        return;
+      }
+
+      const location = await Location.getCurrentPositionAsync();
+      setLocation(location)
+    })()
+
+  }, [])
+
   return (
     <View style={styles.container}>
-       <MapView style={styles.map} />
+       <MapView 
+        style={styles.map}
+        initialRegion={INITIAL_REGION}
+        region={{
+          latitude: location?.coords.latitude!,
+          longitude: location?.coords.longitude!,
+          latitudeDelta: 0.00422,
+          longitudeDelta: 0.00421
+        }}
+        showsUserLocation
+        showsMyLocationButton 
+       />
     </View>
   );
 }
