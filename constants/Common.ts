30662326415857f5a20596887df33a1ecb36c0fb
2wons <013ayu@gmail.com>
2024-06-01 14:42:30 +0800

    fix: change typo from caries class

--- constants/Common.ts
+++ constants/Common.ts
@@ -2,6 +2,6 @@ export const BASE_URL = process.env.EXPO_PUBLIC_API_URL;
 
 /* Caries Classes */
 export const HEALTHY = "-0-Healthy"
-export const INITIAL = "-1-Iniital-Caries"
+export const INITIAL = "-1-Initial-Caries"
 export const MODERATE = "-2-Moderate-Caries"
 export const EXTENSIVE = "-3-Extensive-Caries"
