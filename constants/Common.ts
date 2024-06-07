a85065e57ea181f649611df14c11686c8ea9d82e
2wons <013ayu@gmail.com>
2024-06-08 01:51:52 +0800

    fix: fix reco indents

--- constants/Common.ts
+++ constants/Common.ts
@@ -19,37 +19,37 @@ export const getRecommendation = (counts: ClassCounts, extreme: string) => {
         b. Floss daily to remove plaque between teeth.
         c. Regular dental check-ups and cleanings every 6 months.
         d. Maintain a balanced diet low in sugary snacks and drinks.`,
-        [INITIAL]: `You have  [${counts.initial}] initial stage caries teeth. It is recommended that you do the following.
-        1. Maintain Oral Hygiene:
+        [INITIAL]: `You have ${counts.initial} initial stage caries teeth. It is recommended that you do the following.
+1. Maintain Oral Hygiene:
         a. Continue brushing twice a day with fluoride toothpaste.
         b. Floss daily to remove plaque and prevent further decay.
-        2. Fluoride Use:
+2. Fluoride Use:
         a. Use fluoride mouthwash to help remineralize enamel.
         b. Consider professional fluoride treatments at the dentist's office.
-        3. Dietary Advice:
+3. Dietary Advice:
         a. Reduce the intake of sugary and acidic foods and beverages.
         b. Increase intake of water and foods rich in calcium and phosphates.`,
         [MODERATE]: 
-        `You have [${counts.moderate}] moderate stage caries teeth. It is recommended that you do the following.
-    1. Consult Your Dentist:
+        `You have ${counts.moderate} moderate stage caries teeth. It is recommended that you do the following.
+1. Consult Your Dentist:
     a. Schedule an appointment for a thorough dental examination.
     b. The dentist may perform a professional cleaning and possibly sealants to protect teeth.
-    2. Restorative Treatment:
+2. Restorative Treatment:
     a. Fillings may be necessary to restore the affected areas and prevent further decay.
     b. The dentist might also use fluoride varnish or other remineralizing agents.
-    3. Oral Hygiene and Diet:
+3. Oral Hygiene and Diet:
     a. Maintain rigorous oral hygiene practices.
     b. Follow dietary recommendations to minimize sugar and acid exposure.`,
-        [EXTENSIVE]: `You have [${counts.extensive}] extensive stage caries teeth. It is recommended that you do the following.
-        1. Seek Immediate Dental Treatment:
+        [EXTENSIVE]: `You have ${counts.extensive} extensive stage caries teeth. It is recommended that you do the following.
+1. Seek Immediate Dental Treatment:
             a. Schedule an appointment for urgent dental care to prevent infection and further damage.
             b. The dentist will likely need to perform more extensive restorative work, such as fillings, crowns, or possibly root canal therapy if the pulp is involved.
-        2. Preventive Measures Post-Treatment:
+2. Preventive Measures Post-Treatment:
             a. Follow all post-treatment care instructions provided by the dentist.
             b. Consider additional preventive measures, such as fluoride treatments or dental sealants, to protect other teeth.
-        3. Maintain Oral Hygiene:
+3. Maintain Oral Hygiene:
             a. Continue rigorous oral hygiene practices and regular dental visits to monitor and maintain dental health.
-        4. Dietary Advice:
+4. Dietary Advice:
             a. Follow a diet that supports dental health, emphasizing low sugar and high nutrient foods.`,
         ["none"]: "No Caries Detected"
     }
