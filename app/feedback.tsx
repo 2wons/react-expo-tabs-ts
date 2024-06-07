642a60332a967af26b592969ff8723a83d38c361
2wons <013ayu@gmail.com>
2024-06-07 08:59:15 +0800

    chore: use asset for qr image

--- app/feedback.tsx
+++ app/feedback.tsx
@@ -2,15 +2,15 @@
 import { ExternalLink } from "@/components/ExternalLink";
 import { View } from "@/components/Themed";
 import { StyleSheet } from "react-native";
-import { Text, Button } from "tamagui";
+import { Button } from "tamagui";
 import { MessageSquareShare } from "@tamagui/lucide-icons";
 import { Image } from "react-native";
+
 export default function FeedbackScreen() {
-    const FEEDBACK_QR = "https://i.ibb.co/th4sc7h/Carident-Survey-Student.png"
     const FEEDBACK_LINK = "https://docs.google.com/forms/d/e/1FAIpQLSecwrS5SShcnZqSqctCGgJht_zu1IeZh_DNjyWa1WjeIGzLWA/viewform"
     return (
         <View style={styles.container}>
-            <Image source={{ uri: FEEDBACK_QR }} style={styles.qr} />
+            <Image source={require('@/assets/images/surveyQR.png')} style={styles.qr} />
             <ExternalLink href={FEEDBACK_LINK} asChild>
                 <Button icon={MessageSquareShare}>Open Google Feedback Form</Button>
             </ExternalLink>
