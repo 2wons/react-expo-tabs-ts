09e52d2ce06108a7d08517e172565909a9f9ffb8
2wons <013ayu@gmail.com>
2024-06-16 20:04:16 +0800

    fix: icdas codes

--- constants/Common.ts
+++ constants/Common.ts
@@ -27,15 +27,15 @@ export const icdas = {
         description: "No signs of decay. The tooth looks normal. It’s clean, smooth, and white without any spots or discoloration. ",
     },
     initial: {
-        title: "Initial Caries (ICDAS Code 1)",
+        title: "Initial Caries (ICDAS Code 1-2)",
         description: "These are early signs of tooth decay. You might see some white spots on the tooth if you dry it first. These spots show the very early stage of decay just starting on the surface. You can also see white or light brown spots on the tooth even without drying it. These spots are signs of early decay, but the tooth surface is still mostly intact.",
     },
     moderate: {
-        title: "Moderate Caries (ICDAS Code 2)",
+        title: "Moderate Caries (ICDAS Code 3-4)",
         description: "The tooth has small holes or rough spots where the enamel (the hard outer layer) has started to break down, but you can't see the deeper layer (dentin) yet. You might see a dark shadow under the enamel, indicating the decay is deeper and has reached the dentin (the layer under the enamel). The surface might still look mostly okay or have small breaks.",
     },
     extensive: {
-        title: "Extensive Caries (ICDAS Code 3)",
+        title: "Extensive Caries (ICDAS Code 5-6)",
         description: "There’s a clear hole or cavity in the tooth, and you can see the dentin. The decay has gone through the enamel and reached the deeper layers. For an even more severe case, the tooth has a big, obvious cavity that might be close to or exposing the tooth's inner pulp (the soft part with nerves). There’s significant damage, with a lot of enamel and dentin lost."
     }
 }
